9c3f7e504516fefe4b73cb077f640f9552d75b67
Clinton Dreisbach <clinton@dreisbach.us>
2018-11-20 15:23:24 -0500

    Add login

--- shelter/urls.py
+++ shelter/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -35,6 +35,7 @@ urlpatterns = [
         core_views.register,
         name="register",
     ),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
