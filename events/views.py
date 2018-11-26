e6e0bea85f08dbf8e302fd668d58713300557bd3
Clinton Dreisbach <clinton@dreisbach.us>
2018-11-26 11:06:24 -0500

    Add listing of all events
    Not done:
    * We need to filter events to only show future ones.

--- events/views.py
+++ events/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from events.models import Event
+
 
 # Create your views here.
+def all_events(request):
+    events = Event.objects.all().order_by('date')
+    return render(request, 'events/index.html', {'events': events})
